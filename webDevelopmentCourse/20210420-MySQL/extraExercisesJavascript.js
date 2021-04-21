cfe3faa9a1bbb100928657f17c5f9856837e691d
github shiory602 <suzushioufo@gmail.com>
2021-04-21 16:19:36 -0700

    organized code

--- webDevelopmentCourse/20210420-MySQL/extraExercisesJavascript.js
+++ webDevelopmentCourse/20210420-MySQL/extraExercisesJavascript.js
@@ -120,22 +120,22 @@ console.log(V.getVolume);
 // 6. Write a JavaScript program to sort an array of JavaScript objects.
 // Sample Object :
 
-var library = [
-{
-title: 'The Road Ahead',
-author: 'Bill Gates',
-libraryID: 1254
-},
-{
-title: 'Walter Isaacson',
-author: 'Steve Jobs',
-libraryID: 4264
-},
-{
-title: 'Mockingjay: The Final Book of The Hunger Games',
-author: 'Suzanne Collins',
-libraryID: 3245
-}];
+var library = [{
+    title: 'The Road Ahead',
+    author: 'Bill Gates',
+    libraryID: 1254
+  },
+  {
+    title: 'Walter Isaacson',
+    author: 'Steve Jobs',
+    libraryID: 4264
+  },
+  {
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    author: 'Suzanne Collins',
+    libraryID: 3245
+  }
+];
 
 /* Expected Output:
 [[object Object] {
@@ -173,23 +173,21 @@ title: "Bill Gates"
 // });
 // console.log(tmp);
 
-var sort_by = function(field_name, reverse, initial){
+var sort_by = function (field_name, reverse, initial) {
   var key = initial ?
-  function(x)
-  {
-  return initial(x[field_name]);
-  } :
-  function(x)
-  {
-  return x[field_name];
-  };
+    function (x) {
+      return initial(x[field_name]);
+    } :
+    function (x) {
+      return x[field_name];
+    };
   reverse = !reverse ? 1 : -1;
-  
+
   return function (x, y) {
-  return x = key(x), y = key(y), reverse * ((x &gt; y) - (y &gt;
-  x));
-  } ;
+    return x = key(x), y = key(y), reverse * ((x > y) - (y >
+      x));
   };
-  
-  var newobj = library.sort(sort_by(&#39;libraryID&#39;, true, parseInt));
-  console.log(newobj);
+};
+
+var newobj = library.sort(sort_by('libraryID', true, parseInt));
+console.log(newobj);
