e053c94fd271b994fb8941158ea39ad9f58240bb
github shiory602 <suzushioufo@gmail.com>
2021-04-22 17:04:58 -0700

    create js

---
+++ webDevelopmentCourse/20210422-finalProject/movieTicket/js/main.js
@@ -0,0 +1,11 @@
+let seatCount = document.querySelector('#seatCount');
+let price = document.querySelector('#price');
+let time = document.querySelector('#time');
+let available = document.querySelector('.available');
+let sold = document.querySelector('.sold');
+let selected = document.querySelector('.selected');
+
+seatCount.innerHTML = '3';
+price.innerHTML = `$${30}`;
+time.innerHTML = '20:40';
+
