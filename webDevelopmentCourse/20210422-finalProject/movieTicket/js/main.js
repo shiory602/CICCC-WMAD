f6ebd9b2d7697cd16536e164c3b467748c9fdc00
github shiory602 <suzushioufo@gmail.com>
2021-04-22 18:42:33 -0700

    remake an array of seats and create count function

--- webDevelopmentCourse/20210422-finalProject/movieTicket/js/main.js
+++ webDevelopmentCourse/20210422-finalProject/movieTicket/js/main.js
@@ -1,28 +1,38 @@
 const seatCount = document.querySelector('#seatCount');
 const price = document.querySelector('#price');
 const time = document.querySelector('#time');
-const boxes = document.querySelectorAll('.jsSeat');
+/** @type {HTMLElement[]} */
+const seats = [...document.querySelectorAll('.jsSeat')];
 const available = document.querySelector('.available');
 const sold = document.querySelector('.sold');
 const selected = document.querySelector('.selected');
 
 let seatSum = 0;
+countSelectedSeats();
 let priceSum = 0;
 
 seatCount.innerHTML = seatSum;
 price.innerHTML = `$${priceSum}`;
 time.innerHTML = '20:40';
 
-boxes.forEach(box => box.addEventListener('click', e => {
+seats.forEach(box => box.addEventListener('click', e => {
     /** @type {HTMLElement} */
     const el = e.currentTarget;
-    if(el.classList.contains('sold')) {
+    if (el.classList.contains('sold')) {
         alert('This seat is already sold.');
-    } else if(el.classList.contains('available')) {
+    } else if (el.classList.contains('available')) {
         el.classList.add('selected');
         el.classList.remove('available');
-    } else if(el.classList.contains('selected')) {
+    } else if (el.classList.contains('selected')) {
         el.classList.add('available');
         el.classList.remove('selected');
     }
+    countSelectedSeats();
+    seatCount.innerHTML = seatSum;
+    price.innerHTML = `$${priceSum}`;
 }));
+
+
+function countSelectedSeats() {
+    seatSum = seats.filter(v => v.classList.contains('selected')).length;
+}
