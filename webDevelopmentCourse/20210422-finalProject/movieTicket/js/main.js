4c0081086a86e68332ca3adf58ec7578e3ed834e
github shiory602 <suzushioufo@gmail.com>
2021-04-22 17:54:24 -0700

    button is available

--- webDevelopmentCourse/20210422-finalProject/movieTicket/js/main.js
+++ webDevelopmentCourse/20210422-finalProject/movieTicket/js/main.js
@@ -1,95 +1,7 @@
-// box elements: START //////////////////////////////////
-let A02 = document.querySelector('#A-02');
-let A03 = document.querySelector('#A-03');
-let A04 = document.querySelector('#A-04');
-let A05 = document.querySelector('#A-05');
-let A06 = document.querySelector('#A-06');
-let A07 = document.querySelector('#A-07');
-let A08 = document.querySelector('#A-08');
-let A09 = document.querySelector('#A-09');
-
-let B02 = document.querySelector('#B-02');
-let B03 = document.querySelector('#B-03');
-let B04 = document.querySelector('#B-04');
-let B05 = document.querySelector('#B-05');
-let B06 = document.querySelector('#B-06');
-let B07 = document.querySelector('#B-07');
-let B08 = document.querySelector('#B-08');
-let B09 = document.querySelector('#B-09');
-
-let C01 = document.querySelector('#C-01');
-let C02 = document.querySelector('#C-02');
-let C03 = document.querySelector('#C-03');
-let C04 = document.querySelector('#C-04');
-let C05 = document.querySelector('#C-05');
-let C06 = document.querySelector('#C-06');
-let C07 = document.querySelector('#C-07');
-let C08 = document.querySelector('#C-08');
-let C09 = document.querySelector('#C-09');
-let C10 = document.querySelector('#C-10');
-
-let D01 = document.querySelector('#D-01');
-let D02 = document.querySelector('#D-02');
-let D03 = document.querySelector('#D-03');
-let D04 = document.querySelector('#D-04');
-let D05 = document.querySelector('#D-05');
-let D06 = document.querySelector('#D-06');
-let D07 = document.querySelector('#D-07');
-let D08 = document.querySelector('#D-08');
-let D09 = document.querySelector('#D-09');
-let D10 = document.querySelector('#D-10');
-
-let E01 = document.querySelector('#E-01');
-let E02 = document.querySelector('#E-02');
-let E03 = document.querySelector('#E-03');
-let E04 = document.querySelector('#E-04');
-let E05 = document.querySelector('#E-05');
-let E06 = document.querySelector('#E-06');
-let E07 = document.querySelector('#E-07');
-let E08 = document.querySelector('#E-08');
-let E09 = document.querySelector('#E-09');
-let E10 = document.querySelector('#E-10');
-
-let F01 = document.querySelector('#F-01');
-let F02 = document.querySelector('#F-02');
-let F03 = document.querySelector('#F-03');
-let F04 = document.querySelector('#F-04');
-let F05 = document.querySelector('#F-05');
-let F06 = document.querySelector('#F-06');
-let F07 = document.querySelector('#F-07');
-let F08 = document.querySelector('#F-08');
-let F09 = document.querySelector('#F-09');
-let F10 = document.querySelector('#F-10');
-
-let G01 = document.querySelector('#G-01');
-let G02 = document.querySelector('#G-02');
-let G03 = document.querySelector('#G-03');
-let G04 = document.querySelector('#G-04');
-let G05 = document.querySelector('#G-05');
-let G06 = document.querySelector('#G-06');
-let G07 = document.querySelector('#G-07');
-let G08 = document.querySelector('#G-08');
-let G09 = document.querySelector('#G-09');
-let G10 = document.querySelector('#G-10');
-
-let H01 = document.querySelector('#H-01');
-let H02 = document.querySelector('#H-02');
-let H03 = document.querySelector('#H-03');
-let H04 = document.querySelector('#H-04');
-let H05 = document.querySelector('#H-05');
-let H06 = document.querySelector('#H-06');
-let H07 = document.querySelector('#H-07');
-let H08 = document.querySelector('#H-08');
-let H09 = document.querySelector('#H-09');
-let H10 = document.querySelector('#H-10');
-// box elements: END ///////////////////////////////////////
-
-
-
-
 let seatCount = document.querySelector('#seatCount');
 let price = document.querySelector('#price');
 let time = document.querySelector('#time');
+let boxes = document.querySelectorAll('.box');
 let available = document.querySelector('.available');
 let sold = document.querySelector('.sold');
 let selected = document.querySelector('.selected');
@@ -98,3 +10,6 @@ seatCount.innerHTML = '3';
 price.innerHTML = `$${30}`;
 time.innerHTML = '20:40';
 
+boxes.forEach(box => box.addEventListener('click', e => {
+    console.log('hi');
+}));
