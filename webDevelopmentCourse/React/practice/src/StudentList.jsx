cedf0c4545bf682fa3ccf6b6239812b4f1f1768a
shiory602 <suzushioufo@gmail.com>
2021-05-17 11:45:39 -0700

    solution challenge2

---
+++ webDevelopmentCourse/React/practice/src/StudentList.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+
+class StudentList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            content: <img src={this.props.img} alt='loading' />,
+            listItem: [
+                {
+                    id: 1,
+                    name: 'Alice',
+                },
+                {
+                    id: 2,
+                    name: 'John',
+                },
+                {
+                    id: 3,
+                    name: 'Eddy',
+                },
+            ],
+        }
+    }
+    
+
+    componentDidMount() {
+        this.timer = setInterval(
+            () => this.setState({
+                content: this.state.listItem.map(item => (
+                <li key={item.id}>{item.name}</li>
+                ))
+            }),
+        3000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+        
+    render() {
+        return (
+            <div id='main'>
+                {this.state.content}
+            </div>
+        );
+    }
+}
+
+
+export default StudentList;
--- webDevelopmentCourse/React/practice/src/index.js
+++ webDevelopmentCourse/React/practice/src/index.js
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import ReactDOM from 'react-dom';
 
-import Blog from './Blog';
-// import App from './App';
+// import Blog from './Blog';
+import App from './App';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    {/* <App /> */}
-    <Blog />
+    <App />
+    {/* <Blog /> */}
   </StrictMode>,
   rootElement
 );
