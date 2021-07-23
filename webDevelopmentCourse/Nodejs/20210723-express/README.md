a9e4f8cf56b3e0bc35a61fa0cb8783a19e331202
shiory602 <suzushioufo@gmail.com>
2021-07-23 15:49:57 -0700

    change folda name

---
+++ webDevelopmentCourse/Nodejs/20210723-express/README.md
@@ -0,0 +1,10 @@
+# Express のインストール
+> Node.js と npm が入っていること前提
+インストールしたい先のフォルダでコマンド入力
+```
+npm install -g express
+```
+Express generatorも一緒にインストール
+```
+npm install -g express-generator
+```
--- webDevelopmentCourse/Nodejs/20210723-express/app.js
+++ webDevelopmentCourse/Nodejs/20210723-express/app.js
@@ -1,8 +1,11 @@
 const http = require('http')
+const hostname = '127.0.0.1';
+const port = 3000;
+
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text-plain'});
     response.end('Hello world')
 })
-.listen(8000, () => {
-    console.log('Server running at port 8000');
+.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 })
