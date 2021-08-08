74b16e56b13a153695f91e493eb3b93c3a70edc5
shiory602 <suzushioufo@gmail.com>
2021-08-08 14:19:12 -0700

    assignment

---
+++ WMADーIvan/JavascriptExamPart1/task1.js
@@ -0,0 +1,9 @@
+// You have n tiles, where each tile has one letter tiles[i] printed on it.
+// Return the number of possible non-empty sequences of letters you can make using the letters printed on those tiles.
+
+// 1 <= tiles.length <= 7
+// tiles consists of uppercase English letters.
+
+let tiles = "AAB";
+let tiles = "AAABBC";
+let tiles = "V";
